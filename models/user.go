b40fe64c3b487b6c55ae063cf5ee1ec03d102c56
arceister <imandajagad01@gmail.com>
2022-04-03 23:14:17 +0700

    Feat: Separate Exp

--- models/user.go
+++ models/user.go
@@ -3,17 +3,18 @@ package models
 import "time"
 
 type User struct {
-	ID        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name      string    `gorm:"notNull" json:"name"`
-	Email     string    `gorm:"notNull;uniqueIndex;size:256" json:"email"`
-	NoHP      string    `gorm:"notNull;uniqueIndex;size:30" json:"no_hp"`
-	Password  string    `gorm:"notNull" json:"password"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	XPPoints  uint      `gorm:"default:0" json:"xp_points"`
-	Donate    []Donate  `gorm:"many2many:users_donate;"`
-	Latitude  float64   `gorm:"type:decimal(10,8);default:0" json:"lat"`
-	Longitude float64   `gorm:"type:decimal(11,8);default:0" json:"lng"`
+	ID              uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name            string    `gorm:"notNull" json:"name"`
+	Email           string    `gorm:"notNull;uniqueIndex;size:256" json:"email"`
+	NoHP            string    `gorm:"notNull;uniqueIndex;size:30" json:"no_hp"`
+	Password        string    `gorm:"notNull" json:"password"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	XPPoints        uint      `gorm:"default:0" json:"xp_points"`
+	XPPointsPencari uint      `gorm:"default:0" json:"xp_points_pencari"`
+	Donate          []Donate  `gorm:"many2many:users_donate;"`
+	Latitude        float64   `gorm:"type:decimal(10,8);default:0" json:"lat"`
+	Longitude       float64   `gorm:"type:decimal(11,8);default:0" json:"lng"`
 }
 
 type UserAuthentication struct {
