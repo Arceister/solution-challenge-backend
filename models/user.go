c67520e2cc3daebe1292135e4cf7f717ede24b3e
arceister <imandajagad01@gmail.com>
2022-04-04 06:31:00 +0700

    Feat: Add User `Database` Schema

--- models/user.go
+++ models/user.go
@@ -12,6 +12,8 @@ type User struct {
 	UpdatedAt       time.Time `json:"updated_at"`
 	XPPoints        uint      `gorm:"default:0" json:"xp_points"`
 	XPPointsPencari uint      `gorm:"default:0" json:"xp_points_pencari"`
+	LevelDonatur    string    `gorm:"default:Pejuang Donasi" json:"level_donatur"`
+	LevelPencari    string    `gorm:"default:Pejuang Food Waste" json:"level_pencari"`
 	Donate          []Donate  `gorm:"many2many:users_donate;"`
 	Latitude        float64   `gorm:"type:decimal(10,8);default:0" json:"lat"`
 	Longitude       float64   `gorm:"type:decimal(11,8);default:0" json:"lng"`
